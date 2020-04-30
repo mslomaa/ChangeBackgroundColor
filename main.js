892f9b20ee98258c29ee59a503f714ca775dd000
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-04-30 15:30:48 +0200

    final js

--- main.js
+++ main.js
@@ -6,8 +6,25 @@ let blue = 100;
 document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
 
-const changeColor = () => {
-    
+const changeColor = (e) => {
+    if(e.keyCode === 38){
+        if(red < 256){
+            red = red + 1;
+            green = green + 1;
+            blue = blue + 1; 
+        }
+        document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        console.log(red, green, blue); 
+        
+    } else if(e.keyCode === 40) {
+        if(red > 0){
+            red = red - 1;
+            green = green - 1;
+            blue = blue - 1;
+        }
+        document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        console.log(red, green, blue);
+    }   
 }
 
 window.addEventListener('keydown', changeColor)
