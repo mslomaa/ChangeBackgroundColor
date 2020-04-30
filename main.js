9ff6107e058ce016b994f2724af9ead6c394ba29
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-04-30 14:55:29 +0200

    work in js

---
+++ main.js
@@ -0,0 +1,14 @@
+let red = 100;
+let green = 100;
+let blue = 100;
+
+
+document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+
+const changeColor = () => {
+    
+}
+
+window.addEventListener('keydown', changeColor)
+
